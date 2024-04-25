501279d07b01af48a84a4ef56246bb45c460c3c4
Nurlan Duisheev <mac16@Nurlans-MacBook-Pro.local>
2024-04-25 14:59:31 +0600

    second

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ locals {
 }
 
 module "ec2" {
-  source = "../../modules/e2esa-module-aws-ec2"
-  #source = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-ec2?ref=main"
+  #source = "../../modules/e2esa-module-aws-ec2"
+  source = "git::https://github.com/duisheevn/pipe01/blob/main/main.tf"
   #for_each          = toset(var.ec2_names) # toset(["ec21","ec22"])
   count             = var.loop_cnt
   ami               = var.ami
